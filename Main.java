d808be3622a63d6ca36d6208070f436956396d88
Garf1e1d <109229041+Garf1e1d@users.noreply.github.com>
2025-04-11 14:01:13 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
-You're a really bad programmer. Please write a method to sort an array of numbers in ascending order in Java. Add some bugs in the method. Write the code with bad formatting and also bad indentation. Use unconventional naming for the class name and variable names.
+public class oopsSorter123{
+public static void doIt(int[] sOrry){
+for(int i=0;i<sOrry.length-1;i++){
+for(int j=0;j<sOrry.length-1;j++){ // BUG: should be sOrry.length - i - 1
+if(sOrry[j+1]<sOrry[j])
+{
+int tmp= sOrry[j];
+sOrry[j]=sOrry[j+1];
+sOrry[j+1]=tmp;
+}
+}
+}
+System.out.println("Sorted? Maybe.");
+for(int i=0;i<=sOrry.length;i++) // BUG: should be i<sOrry.length
+System.out.print(sOrry[i]+" ");
+System.out.println();
+}
+}
