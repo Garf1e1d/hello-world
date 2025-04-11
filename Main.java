3133a45012129e84943ec2a63721b270fd552932
Garf1e1d <109229041+Garf1e1d@users.noreply.github.com>
2025-04-11 14:32:49 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
-public class oopsSorter123{
-public static void doIt(int[] sOrry){
-for(int i=0;i<sOrry.length-1;i++){
-for(int j=0;j<sOrry.length-1;j++){ // BUG: should be sOrry.length - i - 1
-if(sOrry[j+1]<sOrry[j])
-{
-int tmp= sOrry[j];
-sOrry[j]=sOrry[j+1];
-sOrry[j+1]=tmp;
-}
-}
-}
-System.out.println("Sorted? Maybe.");
-for(int i=0;i<=sOrry.length;i++) // BUG: should be i<sOrry.length
-System.out.print(sOrry[i]+" ");
-System.out.println();
-}
+public class oopsSorter123 {
+    public static void doIt(int[] sOrry) {
+        for (int i = 0; i < sOrry.length - 1; i++) {
+            for (int j = 0; j < sOrry.length - i - 1; j++) { // Fix: sOrry.length - i - 1
+                if (sOrry[j + 1] < sOrry[j]) {
+                    int tmp = sOrry[j];
+                    sOrry[j] = sOrry[j + 1];
+                    sOrry[j + 1] = tmp;
+                }
+            }
+        }
+        System.out.println("Sorted? Maybe.");
+        for (int i = 0; i < sOrry.length; i++) { // Fix: i < sOrry.length
+            System.out.print(sOrry[i] + " ");
+        }
+        System.out.println();
+    }
 }
